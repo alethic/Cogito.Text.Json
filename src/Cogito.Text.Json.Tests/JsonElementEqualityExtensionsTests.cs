a20ad29add2ab988c1cf5ebef5828c3a2dc5776d
Jerome Haltom <wasabi@cogito.cx>
2019-12-28 13:30:21 -0600

    Added a deep equality method, and an extension to get the property count.

---
+++ src/Cogito.Text.Json.Tests/JsonElementEqualityExtensionsTests.cs
@@ -0,0 +1,197 @@
+﻿using System.Text.Json;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.Text.Json.Tests
+{
+
+    [TestClass]
+    public class JsonElementEqualityExtensionsTests
+    {
+
+        [TestMethod]
+        public void Can_count_empty_object()
+        {
+            var d = JsonDocument.Parse(@"{ }");
+            d.RootElement.GetPropertyCount().Should().Be(0);
+        }
+
+        [TestMethod]
+        public void Can_count_single_object_property()
+        {
+            var d = JsonDocument.Parse(@"{ ""Foo"": ""Bar"" }");
+            d.RootElement.GetPropertyCount().Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Can_count_two_object_property()
+        {
+            var d = JsonDocument.Parse(@"{ ""Foo"": ""Bar"", ""Foo2"": ""Bar2"" }");
+            d.RootElement.GetPropertyCount().Should().Be(2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonException))]
+        public void Should_fail_counting_array()
+        {
+            var d = JsonDocument.Parse(@"[ ""Foo"" ]");
+            d.RootElement.GetPropertyCount();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonException))]
+        public void Should_fail_counting_string()
+        {
+            var d = JsonDocument.Parse(@"""Foo""");
+            d.RootElement.GetPropertyCount();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonException))]
+        public void Should_fail_counting_number()
+        {
+            var d = JsonDocument.Parse(@"1");
+            d.RootElement.GetPropertyCount();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonException))]
+        public void Should_fail_counting_null()
+        {
+            var d = JsonDocument.Parse(@"null");
+            d.RootElement.GetPropertyCount();
+        }
+
+        [TestMethod]
+        public void Can_compare_empty_object()
+        {
+            var a = JsonDocument.Parse(@"{}").RootElement;
+            var b = JsonDocument.Parse(@"{}").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Can_compare_string()
+        {
+            var a = JsonDocument.Parse(@"""foo""").RootElement;
+            var b = JsonDocument.Parse(@"""foo""").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Can_compare_number()
+        {
+            var a = JsonDocument.Parse(@"1").RootElement;
+            var b = JsonDocument.Parse(@"1").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Can_compare_boolean()
+        {
+            var a = JsonDocument.Parse(@"true").RootElement;
+            var b = JsonDocument.Parse(@"true").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Can_compare_decimal_to_integer()
+        {
+            var a = JsonDocument.Parse(@"0.0").RootElement;
+            var b = JsonDocument.Parse(@"0").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_fail_comparing_decimal_to_integer()
+        {
+            var a = JsonDocument.Parse(@"0.1").RootElement;
+            var b = JsonDocument.Parse(@"0").RootElement;
+            a.DeepEquals(b).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_fail_comparing_number_to_string()
+        {
+            var a = JsonDocument.Parse(@"0").RootElement;
+            var b = JsonDocument.Parse(@"""hi""").RootElement;
+            a.DeepEquals(b).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_fail_comparing_number_to_array()
+        {
+            var a = JsonDocument.Parse(@"0").RootElement;
+            var b = JsonDocument.Parse(@"[]").RootElement;
+            a.DeepEquals(b).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_fail_comparing_number_to_object()
+        {
+            var a = JsonDocument.Parse(@"0").RootElement;
+            var b = JsonDocument.Parse(@"{}").RootElement;
+            a.DeepEquals(b).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Can_compare_array_items()
+        {
+            var a = JsonDocument.Parse(@"[1,2,3]").RootElement;
+            var b = JsonDocument.Parse(@"[1,2,3]").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_fail_comparing_unequal_arrays()
+        {
+            var a = JsonDocument.Parse(@"[1,2,3]").RootElement;
+            var b = JsonDocument.Parse(@"[1,2,4]").RootElement;
+            a.DeepEquals(b).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_fail_comparing_unequal_arrays_of_string()
+        {
+            var a = JsonDocument.Parse(@"[1,2,3]").RootElement;
+            var b = JsonDocument.Parse(@"[1,2,""hi""]").RootElement;
+            a.DeepEquals(b).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Can_compare_object_with_properties()
+        {
+            var a = JsonDocument.Parse(@"{ ""Foo"": 1 }").RootElement;
+            var b = JsonDocument.Parse(@"{ ""Foo"": 1 }").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Can_compare_object_with_two_properties()
+        {
+            var a = JsonDocument.Parse(@"{ ""Foo"": 1, ""Bar"": 2 }").RootElement;
+            var b = JsonDocument.Parse(@"{ ""Foo"": 1, ""Bar"": 2 }").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Can_compare_object_with_two_properties_out_of_order()
+        {
+            var a = JsonDocument.Parse(@"{ ""Foo"": 1, ""Bar"": 2 }").RootElement;
+            var b = JsonDocument.Parse(@"{ ""Bar"": 2, ""Foo"": 1 }").RootElement;
+            a.DeepEquals(b).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_fail_comparing_two_objects_with_out_of_order_properties_that_are_not_equal()
+        {
+            var a = JsonDocument.Parse(@"{ ""Foo"": 1, ""Bar"": 2 }").RootElement;
+            var b = JsonDocument.Parse(@"{ ""Bar"": 2, ""Foo"": 2 }").RootElement;
+            a.DeepEquals(b).Should().BeFalse();
+        }
+
+    }
+
+}
---
+++ src/Cogito.Text.Json/JsonElementEqualityExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.Json;
+
+namespace Cogito.Text.Json
+{
+
+    public static class JsonElementEqualityExtensions
+    {
+
+        /// <summary>
+        /// Gets the count of properties present on the element.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static int GetPropertyCount(this JsonElement self)
+        {
+            if (self.ValueKind != JsonValueKind.Object)
+                throw new JsonException("Cannot get property count for non-object.");
+
+            var l = 0;
+            foreach (var i in self.EnumerateObject())
+                l++;
+
+            return l;
+        }
+
+        /// <summary>
+        /// Returns <c>true</c> if the two elements are equal, ignoring object property order.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool DeepEquals(this JsonElement a, JsonElement b)
+        {
+            switch (a.ValueKind)
+            {
+                case JsonValueKind.Undefined:
+                    return b.ValueKind == JsonValueKind.Undefined;
+                case JsonValueKind.Null:
+                    return b.ValueKind == JsonValueKind.Null;
+                case JsonValueKind.False:
+                    return b.ValueKind == JsonValueKind.False;
+                case JsonValueKind.True:
+                    return b.ValueKind == JsonValueKind.True;
+                case JsonValueKind.String:
+                    return b.ValueKind == JsonValueKind.String && a.ValueEquals(b.GetString());
+                case JsonValueKind.Number when a.TryGetInt64(out var l):
+                    return b.ValueKind == JsonValueKind.Number && b.TryGetInt64(out var l2) && l == l2;
+                case JsonValueKind.Number when a.TryGetDouble(out var d):
+                    return b.ValueKind == JsonValueKind.Number && b.TryGetDouble(out var d2) && d == d2;
+                case JsonValueKind.Array:
+                    return b.ValueKind == JsonValueKind.Array && DeepEqualsArray(a, b);
+                case JsonValueKind.Object:
+                    return b.ValueKind == JsonValueKind.Object && DeepEqualsObject(a, b);
+                default:
+                    throw new JsonException("Invalid element type.");
+            }
+        }
+
+        static bool DeepEqualsArray(JsonElement a, JsonElement b)
+        {
+            var al = a.GetArrayLength();
+            var bl = b.GetArrayLength();
+            if (al != bl)
+                return false;
+
+            for (var i = 0; i < al; i++)
+                if (DeepEquals(a[i], b[i]) == false)
+                    return false;
+
+            return true;
+        }
+
+        static bool DeepEqualsObject(JsonElement a, JsonElement b)
+        {
+            var al = GetPropertyCount(a);
+            var bl = GetPropertyCount(b);
+
+            if (al != bl)
+                return false;
+
+            foreach (var ai in a.EnumerateObject())
+                if (!(b.TryGetProperty(ai.Name, out var bv) && DeepEquals(ai.Value, bv)))
+                    return false;
+
+            return true;
+        }
+
+    }
+
+}
