7f85fefe14490750a359eb753cabe7684466a149
Jerome Haltom <wasabi@cogito.cx>
2019-12-27 19:57:23 -0600

    Import.

---
+++ src/Cogito.Json.Tests/JTokenEqualityExpressionBuilderTests.cs
@@ -0,0 +1,193 @@
+﻿using System.IO;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Newtonsoft.Json.Linq;
+
+namespace Cogito.Json.Tests
+{
+
+    [TestClass]
+    public class JTokenEqualityExpressionBuilderTests
+    {
+
+        [TestMethod]
+        public void Should_evaluate_fairly_large_object_as_true()
+        {
+            var o = JObject.Parse(File.ReadAllText(Path.Combine(Directory.GetParent(typeof(JTokenEqualityExpressionBuilderTests).Assembly.Location).FullName, "efm.json")));
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(o);
+            var m = e.Compile();
+            m.Invoke(o).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_evaluate_fairly_large_object_as_false()
+        {
+            var o = JObject.Parse(File.ReadAllText(Path.Combine(Directory.GetParent(typeof(JTokenEqualityExpressionBuilderTests).Assembly.Location).FullName, "efm.json")));
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(o);
+            var m = e.Compile();
+
+            o["Next"] = 123;
+            m.Invoke(o).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_evaluate_simple_object_as_false()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JObject() { ["Foo"] = "Bar" });
+            var m = e.Compile();
+            m.Invoke(new JObject() { ["Foo"] = "Foo" }).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_evaluate_simple_object_as_true()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JObject() { ["Foo"] = "Bar" });
+            var m = e.Compile();
+            m.Invoke(new JObject() { ["Foo"] = "Bar" }).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_evaluate_object_with_two_properties_as_true()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JObject() { ["Foo"] = "Bar", ["Joe"] = 123 });
+            var m = e.Compile();
+            m.Invoke(new JObject() { ["Foo"] = "Bar", ["Joe"] = 123 }).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_evaluate_missing_property_as_false()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JObject() { ["Foo"] = "Bar" });
+            var m = e.Compile();
+            m.Invoke(new JObject() { ["Asd"] = "Bar" }).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_evaluate_extra_property_as_false()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JObject() { ["Foo"] = "Bar" });
+            var m = e.Compile();
+            m.Invoke(new JObject() { ["Foo"] = "Bar", ["Joe"] = 1 }).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_compare_booleans()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JValue(true));
+            var m = e.Compile();
+            m.Invoke(new JValue(true)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_compare_booleans_as_false()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JValue(true));
+            var m = e.Compile();
+            m.Invoke(new JValue(false)).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_compare_strings()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JValue("TEST"));
+            var m = e.Compile();
+            m.Invoke(new JValue("TEST")).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_compare_strings_as_false()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JValue("TEST"));
+            var m = e.Compile();
+            m.Invoke(new JValue("TEST2")).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_compare_empty_arrays()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JArray());
+            var m = e.Compile();
+            m.Invoke(new JArray()).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_compare_arrays_with_one_value()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JArray() { true });
+            var m = e.Compile();
+            m.Invoke(new JArray() { true }).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_compare_arrays_with_one_unequal_value()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JArray() { true });
+            var m = e.Compile();
+            m.Invoke(new JArray() { false }).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_compare_arrays_with_unequal_size()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(new JArray() { true });
+            var m = e.Compile();
+            m.Invoke(new JArray() { true, true }).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Can_compare_against_null()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(JObject.Parse("{'foo': 12}"));
+            var m = e.Compile();
+            m.Invoke(JValue.CreateNull()).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Can_compare_against_badly_typed_properties()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(JObject.Parse("{'foo': 12}"));
+            var m = e.Compile();
+            m.Invoke(JObject.Parse("{'foo': false}")).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Should_compare_integers_as_equal_if_difference_is_decimal_only_ltr ()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(JToken.Parse("0"));
+            var m = e.Compile();
+            m.Invoke(JToken.Parse("0.0")).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_compare_integers_as_equal_if_difference_is_decimal_only()
+        {
+            var b = new JTokenEqualityExpressionBuilder();
+            var e = b.Build(JToken.Parse("0.0"));
+            var m = e.Compile();
+            m.Invoke(JToken.Parse("0")).Should().BeTrue();
+        }
+
+    }
+
+}
---
+++ src/Cogito.Json/JTokenEqualityExpressionBuilder.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using Newtonsoft.Json.Linq;
+
+namespace Cogito.Json
+{
+
+    /// <summary>
+    /// Provides the capability of generating LINQ Expression trees to test whether a <see cref="JToken"/> instance is
+    /// equal with another.
+    /// </summary>
+    public class JTokenEqualityExpressionBuilder
+    {
+
+        static readonly Expression True = Expression.Constant(true);
+        static readonly Expression False = Expression.Constant(false);
+
+        /// <summary>
+        /// Returns an expression that returns <c>true</c> if all of the given expressions returns <c>true</c>.
+        /// </summary>
+        /// <param name="expressions"></param>
+        /// <returns></returns>
+        static Expression AllOf(IEnumerable<Expression> expressions)
+        {
+            Expression e = null;
+
+            foreach (var i in expressions)
+                e = e == null ? i : Expression.AndAlso(e, i);
+
+            return e;
+        }
+
+        /// <summary>
+        /// Returns an expression that returns <c>true</c> if all of the given expressions returns <c>true</c>.
+        /// </summary>
+        /// <param name="expressions"></param>
+        /// <returns></returns>
+        static Expression AllOf(params Expression[] expressions) =>
+            AllOf((IEnumerable<Expression>)expressions);
+
+        /// <summary>
+        /// Builds an expression tree that implements validation of JSON.
+        /// </summary>
+        /// <param name="template"></param>
+        /// <returns></returns>
+        public Expression<Func<JToken, bool>> Build(JToken template)
+        {
+            if (template == null)
+                throw new ArgumentNullException(nameof(template));
+
+            var t = Expression.Parameter(typeof(JToken), "target");
+            var e = Build(template, t);
+            return Expression.Lambda<Func<JToken, bool>>(e, t);
+        }
+
+        /// <summary>
+        /// Builds an expression tree which evaluates whether the <see cref="JToken"/> refered to by <paramref name="target"/>
+        /// is the same as the <paramref name="template"/>.
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public Expression Build(JToken template, Expression target)
+        {
+            var v = Expression.Variable(target.Type);
+
+            return Expression.Block(
+                typeof(bool),
+                new[] { v },
+                Expression.Assign(v, target),
+                Eval(template, v));
+        }
+
+        Expression Eval(JToken template, ParameterExpression target)
+        {
+            if (template == null)
+                throw new ArgumentNullException(nameof(template));
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            switch (template.Type)
+            {
+                case JTokenType.Array:
+                    return BuildArray((JArray)template, target);
+                case JTokenType.Boolean:
+                    return BuildBoolean((JValue)template, target);
+                case JTokenType.Float:
+                    return BuildFloat((JValue)template, target);
+                case JTokenType.Integer:
+                    return BuildInteger((JValue)template, target);
+                case JTokenType.Null:
+                    return BuildNull((JValue)template, target);
+                case JTokenType.Object:
+                    return BuildObject((JObject)template, target);
+                case JTokenType.String:
+                    return BuildString((JValue)template, target);
+                default:
+                    throw new InvalidOperationException("Unsupported token type in template.");
+            }
+        }
+
+        Expression BuildArray(JArray template, Expression target)
+        {
+            return Expression.Condition(
+                Expression.AndAlso(
+                    Expression.TypeIs(target, typeof(JArray)),
+                    Expression.Equal(
+                        Expression.Property(target, nameof(JToken.Type)),
+                        Expression.Constant(JTokenType.Array))),
+                AllOf(BuildArrayEval(template, Expression.Convert(target, typeof(JArray)))),
+                False);
+        }
+
+        /// <summary>
+        /// Iterates an expression that compares each position of each array.
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        IEnumerable<Expression> BuildArrayEval(JArray template, Expression target)
+        {
+            yield return Expression.Equal(
+                Expression.Property(target, nameof(JArray.Count)),
+                Expression.Constant(template.Count));
+
+            for (var i = 0; i < template.Count; i++)
+                yield return Build(template[i], Expression.Property(target, "Item", Expression.Constant(i)));
+        }
+
+        Expression BuildBoolean(JValue template, Expression target)
+        {
+            return BuildValue<bool>(JTokenType.Boolean, template, target);
+        }
+
+        Expression BuildFloat(JValue template, Expression target)
+        {
+            var t = Expression.Convert(target, typeof(JValue));
+
+            return Expression.Switch(
+                Expression.Property(target, nameof(JToken.Type)),
+                False,
+                Expression.SwitchCase(
+                    Expression.Equal(
+                        Expression.Convert(Expression.Property(t, nameof(JValue.Value)), typeof(double)),
+                        Expression.Constant((double)template.Value)),
+                    Expression.Constant(JTokenType.Float)),
+                Expression.SwitchCase(
+                    Expression.Equal(
+                        Expression.Convert(Expression.Convert(Expression.Property(t, nameof(JValue.Value)), typeof(long)), typeof(double)),
+                        Expression.Constant((double)template.Value)),
+                    Expression.Constant(JTokenType.Integer)));
+        }
+
+        Expression BuildInteger(JValue template, Expression target)
+        {
+            var t = Expression.Convert(target, typeof(JValue));
+
+            return Expression.Switch(
+                Expression.Property(target, nameof(JToken.Type)),
+                False,
+                Expression.SwitchCase(
+                    Expression.Equal(
+                        Expression.Convert(Expression.Property(t, nameof(JValue.Value)), typeof(long)),
+                        Expression.Constant((long)template.Value)),
+                    Expression.Constant(JTokenType.Integer)),
+                Expression.SwitchCase(
+                    Expression.Equal(
+                        Expression.Convert(Expression.Property(t, nameof(JValue.Value)), typeof(double)),
+                        Expression.Constant((double)(long)template.Value)),
+                    Expression.Constant(JTokenType.Float)));
+        }
+
+        Expression BuildNull(JValue template, Expression target)
+        {
+            return BuildValue<object>(JTokenType.Null, template, target);
+        }
+
+        Expression BuildObject(JObject template, Expression target)
+        {
+            return Expression.Condition(
+                Expression.AndAlso(
+                    Expression.TypeIs(target, typeof(JObject)),
+                    Expression.Equal(
+                        Expression.Property(target, nameof(JToken.Type)),
+                        Expression.Constant(JTokenType.Object))),
+                AllOf(BuildObjectEval(template, Expression.Convert(target, typeof(JObject)))),
+                False);
+        }
+
+        /// <summary>
+        /// Iterates an expression that compares each position of each array.
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        IEnumerable<Expression> BuildObjectEval(JObject template, Expression target)
+        {
+            yield return Expression.Equal(
+                Expression.Property(target, nameof(JObject.Count)),
+                Expression.Constant(template.Count));
+
+            foreach (var p in template.Properties())
+                yield return Expression.AndAlso(
+                    Expression.IsTrue(
+                        Expression.Call(
+                            target,
+                            nameof(JObject.ContainsKey),
+                            new Type[0],
+                            new[] { Expression.Constant(p.Name) })),
+                    Build(
+                        p.Value,
+                        Expression.Call(
+                            target,
+                            nameof(JObject.GetValue),
+                            new Type[0],
+                            new[] { Expression.Constant(p.Name) })));
+        }
+
+        Expression BuildString(JValue template, Expression target)
+        {
+            return BuildValue<string>(JTokenType.String, template, target);
+        }
+
+        Expression BuildValue<T>(JTokenType type, JValue template, Expression target)
+        {
+            var t = Expression.Convert(target, typeof(JValue));
+            var v = Expression.Convert(Expression.Property(t, nameof(JValue.Value)), typeof(T));
+
+            return Expression.Condition(
+                Expression.AndAlso(
+                    Expression.TypeIs(target, typeof(JValue)),
+                    Expression.Equal(
+                        Expression.Property(target, nameof(JToken.Type)),
+                        Expression.Constant(type))),
+                Expression.Equal(Expression.Constant((T)template.Value), v),
+                False);
+        }
+
+    }
+
+}
